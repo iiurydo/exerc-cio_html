290d362efe6a4c77b9bdc3941eb938ae19de97c9
iiurydo <iurysantos386@yahoo.com.br>
2026-02-07 00:50:06 -0300

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,22 @@
+//Função de multiplicar
+
+function multiplica (numA: number, numB: number): number{
+
+    return numA * numB;
+}
+
+
+console.log(multiplica(5,2));
+
+//Função de saudação
+
+function cumprimenta (){
+    const nome: string = 'Tony Soprano';
+    return `Olá, ${nome}`
+}
+
+console.log(cumprimenta());
+
+
+
+
